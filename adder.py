b8e55bf0879e21420c29e1d235a1c1b4b2ef77a4
Termux Professor <termuxqueenyt@gmail.com>
2020-09-28 10:10:22 +0530

    Update adder.py

--- adder.py
+++ adder.py
@@ -21,7 +21,7 @@ print (re+" ╩ └─┘┴─┘└─┘╚═╝  ╩ ╩─┴┘─┴┘�
 
 print (cy+"version : 1.01")
 print (cy+"Make sure you Subscribed Termux Professor On Youtube")
-print (cy+"www.youtube.com/c/TermuxProfessor")
+print (cy+"www.youtube.com/c/TermuxProfessorYT")
 
 print (re+"NOTE :")
 print ("1. Telegram only allow to add 200 members in group by one user.")
