4886e91506c4cab89af7a39f797c00f963869bb0
dylan2709 <77615706+dylan2709@users.noreply.github.com>
2021-01-19 01:00:06 +0800

    Update adder.py

--- adder.py
+++ adder.py
@@ -118,8 +118,8 @@ for user in users:
         time.sleep(random.randrange(0, 5))
     except PeerFloodError:
         print("Getting Flood Error from telegram. Script is stopping now. Please try again after some time.")
-        print("Waiting {} seconds".format(SLEEP_TIME_2))
-        time.sleep(SLEEP_TIME_2)
+        print("Waiting {} seconds".format(SLEEP.TIME.2))
+        time.sleep(SLEEP.TIME.2)
     except UserPrivacyRestrictedError:
         print("The user's privacy settings do not allow you to do this. Skipping.")
         print("Waiting for 5 Seconds...")
